db41676b51da160e8e2015530fb25e3ae00fc821
nguumiho <minhhoang25498@gmail.com>
2024-09-24 17:40:21 +0700

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,35 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import theme from './theme';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import theme from "./theme";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/mh-coffee/",
+    element: <App />,
+    children: [
+      {
+        path: "/mh-coffee/",
+      },
+      {
+        path: "/mh-coffee/contact",
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
